69de810c0bba249c6486b33a842ca1945a24bc8e
jesus <jesusjsgdev@gmail.com>
2026-01-21 22:22:45 -0400

    feat: scandir function moved to utils file
    New function added to get the file extension

--- src/filesnap/utils.py
+++ src/filesnap/utils.py
@@ -1,5 +1,24 @@
+import os
 import time
+from typing import Generator
 
 
 def format_date(date: int | float) -> str:
     return str(time.ctime(date))
+
+
+def get_extension(file_name: str) -> str:
+    _, ext = os.path.splitext(file_name)
+    return ext.lower() if ext else "Invalid extension"
+
+
+def scandir(path: str, recursive: bool = False) -> Generator[os.DirEntry, None, None]:
+    try:
+        for entry in os.scandir(path):
+            if entry.is_dir():
+                if recursive:
+                    yield from scandir(entry.path, recursive)
+            else:
+                yield entry
+    except PermissionError:
+        pass
