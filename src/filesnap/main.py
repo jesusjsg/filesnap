97b9de48e8c9151a45ebe7808a768af5657a5c09
jesus <jesusjsgdev@gmail.com>
2026-01-21 22:24:26 -0400

    feat: New command to count size and count by file extension
    -Recursive option to get all the files in the path selected

--- src/filesnap/main.py
+++ src/filesnap/main.py
@@ -1,5 +1,6 @@
 import os
-from typing import Annotated, Generator
+from collections import defaultdict
+from typing import Annotated
 
 import typer
 from rich.console import Console
@@ -7,35 +8,33 @@ from rich.filesize import decimal
 from rich.table import Table
 
 from filesnap.decorators import benchmark
-from filesnap.utils import format_date
+from filesnap.utils import format_date, get_extension
 
 console = Console()
-app = typer.Typer()
-
-
-def scandir(path: str, recursive: bool = False) -> Generator[os.DirEntry, None, None]:
-    try:
-        for entry in os.scandir(path):
-            if entry.is_dir():
-                if recursive:
-                    yield from scandir(entry.path, recursive)
-            else:
-                yield entry
-    except PermissionError:
-        pass
+app = typer.Typer(no_args_is_help=True)
 
 
 @app.command()
 @benchmark
 def scan(
-    path: Annotated[str, typer.Argument(help="Path to scan.")] = os.getcwd(),
+    path: Annotated[
+        str,
+        typer.Argument(
+            help="Path to scan.",
+        ),
+    ] = os.getcwd(),
     recursive: Annotated[
-        bool, typer.Option(help="Recursive search to list files in subfolders.")
+        bool,
+        typer.Option(
+            "--recursive", "-r", help="Recursive search to list files in subfolders."
+        ),
     ] = False,
     pretty: Annotated[
         bool,
         typer.Option(
-            help="Pretty table to show all the files. Note: this take more time if the path have a lot files."
+            "--pretty",
+            "-p",
+            help="Pretty table to show all the files. Note: this take more time if the path have a lot files.",
         ),
     ] = False,
 ):
@@ -62,3 +61,37 @@ def scan(
         console.print(table)
 
     console.print(f"Total files found: [bold]{count}[/bold]")
+
+
+@app.command()
+def count(
+    path: Annotated[str, typer.Argument(help="Path to count")] = os.getcwd(),
+    recursive: Annotated[bool, typer.Option("--recursive", "-r")] = False,
+):
+    """Count all the files by extension in the path selected"""
+    info_stats = defaultdict(lambda: {"size": 0, "count": 0})
+
+    if os.path.isdir(path):
+        entries = scandir(path, recursive)
+
+        for entry in entries:
+            if entry.is_file():
+                ext = get_extension(entry.name)
+                file_info = entry.stat()
+
+                info_stats[ext]["size"] += file_info.st_size
+                info_stats[ext]["count"] += 1
+
+    table = Table(title=f"File statistics for {path}")
+    table.add_column("Extension", style="cyan")
+    table.add_column("Size", style="magenta", justify="right")
+    table.add_column("Count", style="green", justify="right")
+
+    sorted_stats = sorted(
+        info_stats.items(), key=lambda item: item[1]["size"], reverse=True
+    )
+
+    for ext, info in sorted_stats:
+        table.add_row(ext, decimal(info["size"]), str(info["count"]))
+
+    console.print(table)
