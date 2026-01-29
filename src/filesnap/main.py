c4c70632fab048958ac16478ed33aaaf6bcffc66
jesus <jesusjsgdev@gmail.com>
2026-01-28 22:55:24 -0400

    feat: New command to clean the path
    - All option include to delete the directory and it's content.
    - Ignore option to apply filters while scanning.

--- src/filesnap/main.py
+++ src/filesnap/main.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 from collections import defaultdict
 from typing import Annotated, Optional
 
 import typer
+from rich import print
 from rich.console import Console
 from rich.filesize import decimal
 from rich.table import Table
@@ -147,14 +149,48 @@ def count(
 @app.command()
 def clean(
     path: str,
-    ignore: Annotated[str, typer.Option("--ignore", "-i")],
-    # TODO: Ignore method will be added to exclude files or dirs
+    recursive: Annotated[
+        bool, typer.Option("--recursive", "-r")
+    ] = False,
+    ignore: Annotated[
+        Optional[str], typer.Option("--ignore", "-i")
+    ] = None,
+    all: Annotated[bool, typer.Option("-all", "-a")] = False,
 ):
     """Clean the path entered"""
     validate_path_exist(path)
 
-    delete = typer.confirm(
-        "Are you sure you want to delete the path content?", abort=True
-    )
+    if all:
+        if typer.confirm(
+            f"Are you sure you want to delete {path}?",
+            abort=True,
+        ):
+            shutil.rmtree(path)
+        print(
+            f"[green]The directory {path} was removed successfully![/green]"
+        )
+        raise typer.Exit()
 
-    # TODO: Add the logic to remove the files or dirs even both
+    typer.confirm(
+        "Are you sure you want to delete the content of the path?",
+        abort=True,
+    )
+    ignore_list = get_ignore_list(ignore)
+
+    entries = scandir(path, recursive, ignore_list)
+
+    track_entries = task_progress(
+        entries, description="Cleaning content..."
+    )
+    for entry in track_entries:
+        if entry.is_file() or entry.is_symlink():
+            os.remove(entry)
+        elif entry.is_dir():
+            try:
+                os.rmdir(entry.path)
+            except OSError:
+                pass
+
+    print(
+        "[green]The content of the path was removed successfully![/green]"
+    )
