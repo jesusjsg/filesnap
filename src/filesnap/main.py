405c193a9257a2e7d27f4ad07f7c311d146aee51
jesus <jesusjsgdev@gmail.com>
2026-02-05 20:47:54 -0400

    feat: entry option help and autocompletion removed

--- src/filesnap/main.py
+++ src/filesnap/main.py
@@ -3,7 +3,12 @@ import typer
 from filesnap.files import app as files_app
 from filesnap.version import app as version_app
 
-app = typer.Typer(no_args_is_help=True)
+app = typer.Typer(
+    no_args_is_help=True,
+    add_completion=False,
+    add_help_option=False,
+    help="A simple CLI to handle your files",
+)
 
 app.add_typer(version_app)
 app.add_typer(files_app)
