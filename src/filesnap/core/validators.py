77926f05af8aa38424cb133f4468869e4619f61f
jesus <jesusjsgdev@gmail.com>
2026-03-06 22:39:10 -0400

    feat: Validation of quality range and print imported from rich

--- src/filesnap/core/validators.py
+++ src/filesnap/core/validators.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
 import typer
+from rich import print
 
 
 def validate_source_path(source_path: Path) -> None:
     if not source_path.exists():
         print(
-            f"[bold red]Error:[/bold red] The path [yellow]{source_path}[/yellow] dosen't exist!"
+            f"[bold red]Error:[/bold red] The path [yellow]{source_path}[/yellow] doesn't exist!"
         )
         raise typer.Exit(code=1)
 
@@ -14,3 +15,11 @@ def validate_source_path(source_path: Path) -> None:
 def validate_destination_path(destination_path: Path) -> None:
     if not destination_path.exists():
         destination_path.mkdir(parents=True)
+
+
+def validate_quality(quality: int) -> None:
+    if quality > 100:
+        print(
+            "[bold red]Error:[/bold red] Quantity cannot be greater than 100!"
+        )
+        raise typer.Exit(code=1)
